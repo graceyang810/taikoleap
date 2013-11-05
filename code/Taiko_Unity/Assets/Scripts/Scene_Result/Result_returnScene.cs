6baa27bdf3ef81f59accb488ed1ce54fb6418e31
graceyang810 <grace@gracedeiMac.local>
2013-11-05 17:03:06 +0800

    增加积分功能并在Result界面显示

---
+++ code/Taiko_Unity/Assets/Scripts/Scene_Result/Result_returnScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Result_returnScene : MonoBehaviour {
+
+	public string levelName;
+	
+	void Start()
+	{
+	}
+	
+	void Update()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{			
+			Application.LoadLevel(levelName);
+			ResultData.clearData();
+		}
+		
+	}
+}
